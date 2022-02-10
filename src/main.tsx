9f514b6a95fdb4c561cc2af8ea7904c989a9fa38
Alex Keating <keating.dev@protonmail.com>
2022-02-10 14:16:27 -0500

    Get build to work

--- src/main.tsx
+++ src/main.tsx
@@ -9,15 +9,71 @@ import { CeramicProvider } from "context/CeramicContext";
 import { store } from "store";
 import { Provider } from "react-redux";
 
+import { WalletProvider } from "@raidguild/quiver";
+
+// If using Portis provider
+// If using Frame provider
+// If using wallet connect
+import WalletConnectProvider from "@walletconnect/ethereum-provider";
+import { IProviderOptions } from "web3modal";
+
+const networks = {
+  1: {
+    chainId: 1,
+    name: "Mainnet",
+    symbol: "ETH",
+    explorer: "https://etherscan.io",
+    rpc: "https://mainnet.infura.io/v3/<your infura project id>",
+  },
+  4: {
+    chainId: 4,
+    name: "Rinkeby",
+    symbol: "ETH",
+    explorer: "https://rinkeby.etherscan.io",
+    rpc: "https://rinkeby.infura.io/v3/<your infura project id>",
+  },
+};
+
+const providerOptions: IProviderOptions = {
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      rpc: {
+        1: networks[1].rpc,
+        4: networks[4].rpc,
+      },
+    },
+  },
+};
+const web3modalOptions = {
+  cacheProvider: true,
+  providerOptions,
+  theme: "dark",
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CeramicProvider>
-          <Routes />
-        </CeramicProvider>
-      </ThemeProvider>
-      <GlobalStyle />
+      <WalletProvider
+        web3modalOptions={web3modalOptions}
+        networks={networks}
+        defaultNetwork={1}
+        // Optional but useful to handle events.
+        handleModalEvents={(eventName, error) => {
+          if (error) {
+            console.error(error.message);
+          }
+
+          console.log(eventName);
+        }}
+      >
+        <ThemeProvider theme={theme}>
+          <CeramicProvider>
+            <Routes />
+          </CeramicProvider>
+        </ThemeProvider>
+        <GlobalStyle />
+      </WalletProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
