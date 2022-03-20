3a0df723fea915a3979f86e9c37112fae19bed1b
jonathanprozzi <jonathanprozzi@gmail.com>
2022-03-20 17:26:46 -0400

    HeroSection done for desktop

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import Button from "components/Button";
+import Title from "components/Title";
 import Text from "components/Text";
 import { AppWrapper, Layout, BodyContainer } from "components/Layout";
 import LogoWordmark from "components/LogoWordmark";
@@ -10,6 +11,8 @@ import HomeNav from "components/HomeNav";
 import FAQSection from "components/FAQSection";
 import MobileNav from "components/MobileNav";
 import { DASHBOARD_URI } from "../constants";
+import heroImage from "assets/img-hero.svg";
+import heroImageMobile from "assets/img-hero-sm.svg";
 
 const StyledLayout = styled(Layout)`
   grid-template:
@@ -30,6 +33,8 @@ const StyledMainWrapper = styled(AppWrapper)`
 
 const StyledBodyContainer = styled(BodyContainer)`
   grid-area: body;
+  align-self: flex-start;
+  width: 100%;
 `;
 
 const HomeHeaderContainer = styled.div`
@@ -45,57 +50,65 @@ const HomeHeaderContainer = styled.div`
 const HeroContainer = styled(BodyContainer)`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 1.6rem;
+  padding: 4.8rem;
+  width: 100%;
+  min-height: 80vh;
+  background-image: url(${heroImage});
+  background-position: "top left";
+  background-size: contain;
+  background-repeat: no-repeat;
+  @media (max-width: 768px) {
+    padding: 2.4rem;
+  }
 `;
 
 const HeroCTAContainer = styled.div`
   display: flex;
   flex-direction: column;
-  margin: 0rem 15rem;
-  justify-content: center;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: flex-start;
   @media (max-width: 768px) {
     margin: 0;
   }
 `;
 
-const HeroHeading = styled.h2`
-  font-size: 96px;
+const HeroHeading = styled(Title)`
+  /* font-size: 96px;
   line-height: 102px;
   font-weight: 900;
   text-align: center;
   color: black;
   margin-bottom: 3.2rem;
-  margin-top: 0px;
+  margin-top: 0px; */
   @media (max-width: 768px) {
     font-size: 48px;
     line-height: 60px;
   }
 `;
 
-const HeroTagline = styled.span`
-  font-size: 32px;
+const HeroTagline = styled(Title)`
+  /* font-size: 32px;
   line-height: 38px;
   font-weight: 600;
   text-align: center;
   color: black;
-  margin-bottom: 5.6rem;
+  margin-bottom: 5.6rem; */
 `;
 
 const HeroButton = styled(Button)`
-  border-radius: 8px;
-  width: 26.5rem;
-  height: 6.2rem;
+  margin-top: 3.2rem;
 `;
 
 const LogoContainer = styled.div`
   direction: flex;
   flex-direction: column;
-width:100%;
-  padding:0;
+  width: 100%;
+  padding: 0;
   margin: 0;
   @media (max-width: 768px) {
-p
   }
 `;
 
@@ -157,17 +170,24 @@ const HomePage = () => {
         <HomeNav />
       </HomeHeaderContainer>
       <StyledBodyContainer>
-        {/* <HeroContainer>
+        <HeroContainer>
           <HeroCTAContainer>
-            <HeroHeading>The Decentralized Newsletter.</HeroHeading>
-            <HeroTagline>Your Content, Your Readers.</HeroTagline>
+            <HeroHeading size="md" color="primary">
+              The Decentralized Publishing Platform.
+            </HeroHeading>
+            <HeroTagline size="xxxl" color="primary">
+              Your Content,
+            </HeroTagline>
+            <HeroTagline size="xxxl" color="primary">
+              Your Community.
+            </HeroTagline>
             <Link to={DASHBOARD_URI}>
-              <HeroButton color="primary" variant="contained" size="xl">
+              <HeroButton color="primary" variant="outlined" size="xl">
                 Get Started
               </HeroButton>
             </Link>
           </HeroCTAContainer>
-        </HeroContainer> */}
+        </HeroContainer>
       </StyledBodyContainer>
       <FAQSection />
       <HomeFooterContainer>
