375167569ac473e7898b90e5e41bde5ac4b4485b
Alex Keating <keating.dev@protonmail.com>
2022-03-29 22:18:49 -0400

    Add first toast

--- src/components/Share.tsx
+++ src/components/Share.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEvent } from "react";
+import { useSnackbar } from "notistack";
 import { DOMAIN } from "../constants";
 import share from "assets/share.svg";
 
@@ -7,8 +8,11 @@ type Props = {
 };
 
 const Share = ({ to }: Props) => {
+  const { enqueueSnackbar } = useSnackbar();
+
   const handleClick = (e: MouseEvent<HTMLDivElement>) => {
     e.preventDefault();
+    enqueueSnackbar("Copied", { variant: "success" });
     navigator.clipboard.writeText(`${DOMAIN}/#${to}`);
   };
 
