f77bee750c89048bc6a837074ac918c3dca84ca5
jonathanprozzi <jonathanprozzi@gmail.com>
2022-03-29 22:23:39 -0400

    Added styles for the CreatePublication success splash / celebration for Desktop -- removed redundant styles for mobile/tablet

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -70,7 +70,6 @@ export const HeaderContainer = styled.div`
   grid-area: header;
   display: flex;
   align-items: center;
-
   @media (max-width: 990px) {
     flex-direction: column;
     align-items: flex-start;
--- src/pages/CreatePublicationPage.tsx
+++ src/pages/CreatePublicationPage.tsx
@@ -63,11 +63,12 @@ const StyledText = styled(Text)`
 `;
 
 const WritingActionsContainer = styled.div`
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: column;
-    gap: 1.6rem;
-  }
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  gap: 1.6rem;
 `;
 
 const StyledHeaderContainer = styled(HeaderContainer)`
@@ -76,6 +77,8 @@ const StyledHeaderContainer = styled(HeaderContainer)`
 
 const StyledIcon = styled(Icon)`
   height: 8rem;
+  width: 100%;
+  margin-bottom: 3.2rem;
 `;
 
 const ContentContainer = styled(BodyContainer)`
@@ -110,6 +113,7 @@ const BodyHeaderContainer = styled.div`
   width: 100%;
   margin-top: 6.4rem;
   margin-bottom: 4rem;
+  gap: 0.8rem;
   @media (max-width: 768px) {
     margin-top: 3.2rem;
     padding: 0 2.4rem;
